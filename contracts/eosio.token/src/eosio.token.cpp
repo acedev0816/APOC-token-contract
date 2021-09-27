72c3a301662a0e435d33c539e34ca7dcae0c6c9c
Nero Zato <happyfisherod@gmail.com>
2021-09-27 22:16:52 +0900

    Add create function

---
+++ contracts/eosio.token/src/eosio.token.cpp
@@ -0,0 +1,26 @@
+#include <eosio.token/eosio.token.hpp>
+
+namespace eosio {
+
+void token::create( const name&   issuer,
+                    const asset&  maximum_supply )
+{
+    require_auth( get_self() );
+
+    auto sym = maximum_supply.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( maximum_supply.is_valid(), "invalid supply");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing == statstable.end(), "token with symbol already exists" );
+
+    statstable.emplace( get_self(), [&]( auto& s ) {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+    });
+}
+
+} /// namespace eosio
